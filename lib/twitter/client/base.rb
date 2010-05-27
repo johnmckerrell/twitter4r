4c7759f5adfaec42dfc0b73d8dff01cacbab1e2c
seth <seth@dev03.(none)>
2010-05-27 12:43:24 -0700

    new http request methods which support either oauth or non-authenticated access.
    OAuth authentication presumes that you are using the OAuth gem and have a valid OAuth::AccessToken object (see twitter api documentation for a walkthrough of the oauth process and various tutorials on using the OAuth gem in particular.  I didn't have to do anything tricky with the oauth gem to get it working.)
    
    Pass your AccessToken to the Twitter::Client initializer and it will use those credentials for any api calls switched over to the new methods (all of them as soon as I am done)

--- lib/twitter/client/base.rb
+++ lib/twitter/client/base.rb
@@ -8,6 +8,7 @@ class Twitter::Client
 
   protected
     attr_accessor :login, :password
+    attr_accessor :oauth_token
     
     # Returns the response of the HTTP connection.  
     def http_connect(body = nil, require_auth = true, service = :rest, &block)
@@ -98,5 +99,78 @@ class Twitter::Client
     def create_http_delete_request(uri, params = {})
     	path = (params.size > 0) ? "#{uri}?#{params.to_http_str}" : uri
     	Net::HTTP::Delete.new(path, http_header)
+    end
+
+    def http_request(method, path, body = {}, params = {}, service = :rest)
+
+      response = nil
+      query = params.to_http_str unless params.blank?
+
+      case service
+      when :rest
+        protocol, host, port = @@config.protocol, @@config.host, @@config.port
+      when :search
+        protocol, host, port = @@config.search_protocol, @@config.search_host, @@config.search_port
+      end
+
+      if @oauth_token
+        # If we have an OAuth::AccessToken passed to the Twitter::Client initializer then all http calls need to go 
+        # through the token object and return a Net::HTTPResponse object just like we would get interacting with
+        # Net::HTTP directly.
+
+        case protocol
+        when :ssl
+          builder = URI::HTTPS
+        when :http
+          builder = URI::HTTP
+        end
+        
+        url = builder.build( :host => host, :port => port, :path => path, :query => query ).to_s
+        args = [method, url]
+        args << body if [:post, :put].include?(method)
+        args << http_header
+        response = @oauth_token.send(*args)
+      else
+        # otherwise we can do a non-authenticated http request directly
+
+        conn = Net::HTTP.new(host, port, 
+                              @@config.proxy_host, @@config.proxy_port,
+                              @@config.proxy_user, @@config.proxy_pass)
+        if protocol == :ssl
+          conn.use_ssl = true
+          conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+       
+        # Add timeout from config, if applicable.
+        if @@config.timeout
+          conn.read_timeout = @@config.timeout
+        end
+
+        path = "#{path}?#{query}" if query
+
+        request = Net::HTTP.const_get(method.to_s.capitalize).new(path, headers)
+
+        if body.is_a?(Hash) && !body.blank?
+          request.set_form_data(body)
+        elsif body
+          request.body = body
+        end
+
+        response = conn.request(request)
+      end
+
+      response
+    end
+
+    def http_get_request(uri, params = {}, service = :rest)
+      http_request(:get, uri, nil, params, service)
+    end
+
+    def http_post_request(uri, body, params = {}, service = :rest)
+      http_request(:post, uri, body, params, service)
+    end
+
+    def http_delete_request(uri, params = {}, service = :rest)
+      http_request(:delete, uri, nil, params, service)
     end
 end
