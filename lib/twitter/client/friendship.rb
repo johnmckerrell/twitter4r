50ce3b9d83e32bb4bf2d012712777fdd8ca02218
John McKerrell <john@mckerrell.net>
2010-08-28 14:25:40 +0100

    Fixing friendship and adding show action

--- lib/twitter/client/friendship.rb
+++ lib/twitter/client/friendship.rb
@@ -2,6 +2,7 @@ class Twitter::Client
   @@FRIENDSHIP_URIS = {
     :add => '/friendships/create',
     :remove => '/friendships/destroy',
+    :show => '/friendships/show',
   }
 	
   # Provides access to the Twitter Friendship API.
@@ -28,8 +29,19 @@ class Twitter::Client
   def friend(action, value)
     raise ArgumentError, "Invalid friend action provided: #{action}" unless @@FRIENDSHIP_URIS.keys.member?(action)
     value = value.to_i unless value.is_a?(String)
-    uri = "#{@@FRIENDSHIP_URIS[action]}/#{value}.json"
-    response = http_post_request(uri)
-    bless_model(Twitter::User.unmarshal(response.body))
+    uri = "#{@@FRIENDSHIP_URIS[action]}.json"
+    params = {}
+    if value.is_a?(String)
+      params["target_screen_name"] = value
+    else
+      params["target_id"] = value
+    end
+    if action == :show
+      response = http_get_request(uri,params)
+      JSON.parse(response.body)
+    else
+      response = http_post_request(uri,nil,params)
+      bless_model(Twitter::User.unmarshal(response.body))
+    end
   end
 end
